a7c3f200912375772636d6f30eec4a7f4b470c80
Adam Eivy <Adam.Eivy@disney.com>
2018-05-10 12:36:41 -0700

    rubber underpants in case agent is passed undefined (found some valid traffic) and adding new bots

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const botlist = require('./data/bots')
 const botRegExp = new RegExp('(' + botlist.join('|') + ')', 'ig')
 
 module.exports = function isBot(ua) {
-  return !!ua.match(botRegExp)
+  return !!(ua||'').match(botRegExp)
 }
